c59cc68a3024724520832964f850b7a2014ef06f
MagicKoffe <83956712+MagicKoffe@users.noreply.github.com>
2026-02-05 00:06:47 +0000

    Player shooting aim added
    Added basic aiming for the player

--- Assets/Scripts/Player scripts/PlayerShooting.cs
+++ Assets/Scripts/Player scripts/PlayerShooting.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
+    Transform playerReticle;
+    Vector3 aimDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerReticle = GameObject.FindGameObjectWithTag("Reticle").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        getAimDirection();
+    }
+
+    //Calculate aim direction by subtracting reticel position - player position
+    private void getAimDirection()
+    {
+        aimDirection = (playerReticle.position - transform.position).normalized;
+        Debug.DrawLine(transform.position, aimDirection * 300);
     }
 }
